9ee27105dd3fddae5ccc49dc5073f683ac3b32f8
Cristiano Castro <cristianodmtsb@gmail.com>
2020-02-06 08:58:46 -0300

    Favoritar e desfavoritar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import api from "../../services/api";
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
 import * as FavoriteActions from "../../store/modules/Favorite/actions";
 import { BeerList } from "./styles";
@@ -18,13 +18,21 @@ class Home extends Component {
     this.setState({ beers: response.data });
   }
 
-  handleFavorite = beer => {
+  handleFavor = beer => {
     const { addFavorite } = this.props;
 
     this.setState(addFavorite(beer));
   };
 
+  handleDisfavor = beer => {
+    const { removeFavorite } = this.props;
+
+    this.setState(removeFavorite(beer));
+  };
+
   render() {
+    const { favoriteCheck } = this.props;
+
     return (
       <BeerList>
         {this.state.beers.map(beer => (
@@ -33,12 +41,22 @@ class Home extends Component {
             <strong>{beer.name}</strong>
             <span>First Brewd: {beer.first_brewed}</span>
             <span>{beer.tagline}</span>
-            <button onClick={() => this.handleFavorite(beer)}>
-              <div>
-                <MdFavoriteBorder size={18} />
-              </div>
-              <span>Favoritar</span>
-            </button>
+
+            {favoriteCheck[beer.id] === beer.id ? (
+              <button onClick={() => this.handleDisfavor(beer.id)}>
+                <div>
+                  <MdFavorite size={18} />
+                </div>
+                <span>Desfavoritar</span>
+              </button>
+            ) : (
+              <button onClick={() => this.handleFavor(beer)}>
+                <div>
+                  <MdFavoriteBorder size={18} />
+                </div>
+                <span>Favoritar</span>
+              </button>
+            )}
           </li>
         ))}
       </BeerList>
@@ -46,7 +64,14 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  favoriteCheck: state.favorite.reduce((favorite, beer) => {
+    favorite[beer.id] = beer.id;
+    return favorite;
+  }, {})
+});
+
 const mapDispatchToProps = dispatch =>
   bindActionCreators(FavoriteActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
